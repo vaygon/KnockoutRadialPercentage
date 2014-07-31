240f0abf6ba39a761e6afa4ee0251dcbbc3b4bc3
Vaygon <leemargiotta@gmail.com>
2014-07-31 22:09:08 +0100

    First Commit

---
+++ RadialProgressIndicator/Controllers/RadialController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RadialProgressIndicator.Controllers
+{
+    public class RadialController : Controller
+    {
+        //
+        // GET: /Radial/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ RadialProgressIndicator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RadialProgressIndicator.Startup))]
+namespace RadialProgressIndicator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
